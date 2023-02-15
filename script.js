0b5d393b5f5c84b0a8d97df095f8cfb18c9a6caa
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-02-15 16:05:56 -0500

    commiting notes to js file

---
+++ script.js
@@ -0,0 +1,65 @@
+// var, let, const,
+
+
+console.log()
+
+let score;
+
+score = 10;
+
+console.log(score);
+
+const age= 30;
+
+age= 31;
+
+console.log(age);
+
+//data types
+// strings, numbers, boolean, null, undefined, symbol,
+
+const name='john';
+const age= 30;
+const rating=4.5;
+const isCool=true;
+const x=null; //null= empty nothing in it
+const y = undefined;
+let z;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof rating);
+console.log(typeof isCool);
+console.log(typeof x);
+console.log(typeof y);
+
+
+//strings:
+const name='john';
+const age= 30;
+
+//Concatenation
+console.log('My name is' + name + ' i am age' + age);
+
+//Template string
+const hello=('My name is ${name} and i am ${age}');
+
+
+//Sting Properties and Methods
+
+const s = 'Hello World!'
+
+console.log(s.toLowerCase)
+console.log(s.substring(0,5)).toUpperCase());
+// console.log(s.lenght)
+ //array
+ console.log(s.split(''))
+
+
+ const s = 'technology, computers, it, code';
+ console.log(s.split(', '))
+
+
+
+
+
